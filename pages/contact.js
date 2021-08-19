555ec818c6f9544162f5d50276eff4a6900085c0
Garv Jain <garvj03@gmail.com>
2021-08-20 01:45:07 +0530

    feat: Update Pages

--- pages/contact.js
+++ pages/contact.js
@@ -91,19 +91,14 @@ export default function Contact() {
                 <div className="p-2 w-full">
                   <button
                     type="reset"
-                    className="flex mx-auto text-white bg-indigo-700 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                    className="transition duration-500 ease-in-out mx-auto flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-500 transform hover:-translate-y-1 hover:scale-110 rounded text-lg"
                   >
                     Submit
                   </button>
                 </div>
                 <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                  <p className="leading-normal my-5">
-                    49 Smith St.
-                    <br />
-                    Saint Cloud, MN 56301
-                  </p>
                   <span className="inline-flex">
-                    <a className="text-gray-500">
+                    <span className="text-gray-500">
                       <svg
                         fill="currentColor"
                         strokeLinecap="round"
@@ -114,8 +109,8 @@ export default function Contact() {
                       >
                         <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                       </svg>
-                    </a>
-                    <a className="ml-4 text-gray-500">
+                    </span>
+                    <span className="ml-4 text-gray-500">
                       <svg
                         fill="currentColor"
                         strokeLinecap="round"
@@ -126,8 +121,8 @@ export default function Contact() {
                       >
                         <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                       </svg>
-                    </a>
-                    <a className="ml-4 text-gray-500">
+                    </span>
+                    <span className="ml-4 text-gray-500">
                       <svg
                         fill="none"
                         stroke="currentColor"
@@ -147,8 +142,8 @@ export default function Contact() {
                         ></rect>
                         <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                       </svg>
-                    </a>
-                    <a className="ml-4 text-gray-500">
+                    </span>
+                    <span className="ml-4 text-gray-500">
                       <svg
                         fill="currentColor"
                         strokeLinecap="round"
@@ -159,7 +154,7 @@ export default function Contact() {
                       >
                         <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                       </svg>
-                    </a>
+                    </span>
                   </span>
                 </div>
               </div>
