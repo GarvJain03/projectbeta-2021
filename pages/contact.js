87aafbbff3a469eb695b31e0baf6b9df04668b4a
Garv Jain <garvj03@gmail.com>
2021-08-19 09:33:12 +0530

    feat: Create Pages

--- pages/contact.js
+++ pages/contact.js
@@ -9,24 +9,38 @@ export default function Contact() {
     <div>
       <title>CONTACT</title>
       <Header />
-      <section className="text-gray-600 body-font relative">
+      <section
+        className="text-gray-600 body-font relative"
+        style={{ minHeight: "84vh" }}
+      >
         <form>
-        <div className="container px-5 py-20 mx-auto">
-          <div className="flex flex-col text-center w-full mb-12">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-              Contact Us
-            </h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify.
-            </p>
-          </div>
-          <div className="lg:w-1/2 md:w-2/3 mx-auto">
-            <div className="flex flex-wrap -m-2">
+          <div className="container px-5 py-12 mx-auto">
+            <div className="flex flex-col text-center w-full mb-12">
+              <h1
+                className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900"
+                data-aos="slide-up"
+                data-aos-duration="1500"
+              >
+                Contact Us
+              </h1>
+              <p
+                className="lg:w-2/3 mx-auto leading-relaxed text-base"
+                data-aos="slide-up"
+                data-aos-duration="1550"
+              >
+                Contact us for any grievances or queries.
+              </p>
+            </div>
+            <div
+              className="lg:w-1/2 md:w-2/3 mx-auto"
+              data-aos="slide-up"
+              data-aos-duration="1700"
+            >
+              <div className="flex flex-wrap -m-2">
                 <div className="p-2 w-1/2">
                   <div className="relative">
                     <label
-                      for="name"
+                      htmlFor="name"
                       className="leading-7 text-sm text-gray-600"
                     >
                       Name
@@ -42,7 +56,7 @@ export default function Contact() {
                 <div className="p-2 w-1/2">
                   <div className="relative">
                     <label
-                      for="email"
+                      htmlFor="email"
                       className="leading-7 text-sm text-gray-600"
                     >
                       Email
@@ -58,7 +72,7 @@ export default function Contact() {
                 <div className="p-2 w-full">
                   <div className="relative">
                     <label
-                      for="message"
+                      htmlFor="message"
                       className="leading-7 text-sm text-gray-600"
                     >
                       Message
@@ -71,80 +85,82 @@ export default function Contact() {
                   </div>
                 </div>
                 <div className="p-2 w-full">
-                  <button type="reset" className="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">
+                  <button
+                    type="reset"
+                    className="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg"
+                  >
                     Submit
                   </button>
                 </div>
-              <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                <a className="text-green-500">example@email.com</a>
-                <p className="leading-normal my-5">
-                  49 Smith St.
-                  <br />
-                  Saint Cloud, MN 56301
-                </p>
-                <span className="inline-flex">
-                  <a className="text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <rect
-                        width="20"
-                        height="20"
-                        x="2"
-                        y="2"
-                        rx="5"
-                        ry="5"
-                      ></rect>
-                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                    </svg>
-                  </a>
-                  <a className="ml-4 text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                    </svg>
-                  </a>
-                </span>
+                <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                  <p className="leading-normal my-5">
+                    49 Smith St.
+                    <br />
+                    Saint Cloud, MN 56301
+                  </p>
+                  <span className="inline-flex">
+                    <a className="text-gray-500">
+                      <svg
+                        fill="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                      </svg>
+                    </a>
+                    <a className="ml-4 text-gray-500">
+                      <svg
+                        fill="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                      </svg>
+                    </a>
+                    <a className="ml-4 text-gray-500">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <rect
+                          width="20"
+                          height="20"
+                          x="2"
+                          y="2"
+                          rx="5"
+                          ry="5"
+                        ></rect>
+                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                      </svg>
+                    </a>
+                    <a className="ml-4 text-gray-500">
+                      <svg
+                        fill="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                      </svg>
+                    </a>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
         </form>
       </section>
       <Footer />
---
+++ pages/gallery.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Gallery from "../components/Gallery";
+
+export default function GalleryPage() {
+  return (
+    <div>
+      <Header />
+        <Gallery embedId="JLmOteqmDYc" />
+        <Gallery embedId="8tjMM67-aao"/>
+        <Gallery embedId="0YiirWsRlQ8"/>
+      <Footer />
+    </div>
+  );
+}
+
