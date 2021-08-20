54dc5b66002b94e933f4aec3e7153d9e86ea3a95
Garv Jain <garvj03@gmail.com>
2021-08-20 14:51:33 +0530

    Removed Animations

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,6 @@ function Header() {
     <div className="headerelements">
       <header
         className="nav text-gray-600 body-font bg-white"
-        data-aos="slide-up"
-        data-aos-duration="1000"
       >
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <span className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
